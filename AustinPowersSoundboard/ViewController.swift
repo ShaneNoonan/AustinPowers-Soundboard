a06900c1fc578d4680b8b941983c3dd220b5f185
ShaneNoonan <ShaneNoonan@Shanes-MacBook-Pro.local>
2016-10-05 17:13:38 +0100

    Austin Powers Soundboard app first commit

--- AustinPowersSoundboard/ViewController.swift
+++ AustinPowersSoundboard/ViewController.swift
@@ -7,19 +7,51 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    let soundNames = ["yababy", "smashingbaby", "myself", "groovy-baby", "oh-behave", "i-ate-a-baby"]
+    var audioPlayers = [AVAudioPlayer]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //setup audioplayers
+        for sound in soundNames{
+            
+            do {
+                //try do something
+                let url = NSURL(fileURLWithPath: Bundle.main.path(forResource: sound, ofType: "mp3")!)
+                let audioPlayer = try AVAudioPlayer(contentsOf: url as URL)
+                
+                audioPlayers.append(audioPlayer)
+            }
+                
+                
+            catch {
+                //catch error that is thrown
+                audioPlayers.append(AVAudioPlayer())
+            }
+        }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //sound button tapped
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        //get audioplayer for correct button
+        let audioPlayer = audioPlayers[sender.tag]
+        audioPlayer.play()
+    }
+    
 }
+
+
 
